18a9e4ddeb30e97c9b37bc95cf042dd40c999bbf
Renier Vargas <rvargas@fenacrep.org>
2023-01-09 13:39:19 -0500

    avances con la seccion del DEALCARD

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { card } from '../assets';
+import styles , {layout} from '../style';
+import Button from './Button';
 
 function CardDeal() {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      {/* esta parte contiene el texto que esta del lado izquierdo */}
+      <div className={layout.sectionInfo}> 
+        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden' /> in few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
+        <Button styles="mt-10"/>
+      </div>
+
+      {/* esta parte contiene a la imagen que esta del lado derecho  */}
+      <div className={layout.sectionImg}>
+        <img src={card} alt="deal_card" className='w-[100%] h-[100%] object-contain ' />
+      </div>
+      
+    </section>
   )
 }
 
