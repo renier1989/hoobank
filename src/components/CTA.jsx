40ebddf1587a0caedd51a00035caaa4020877bbf
Renier Vargas <rvargas@fenacrep.org>
2023-01-10 11:52:39 -0500

    avances con la seccion de CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import styles from "../style"
+import Button from "./Button"
 
 function CTA() {
   return (
-    <div>CTA</div>
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow` }>
+      <div className="flex-1 flex flex-col ">
+        <h2 className={styles.heading2}>Let’s try our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+      </div>
+      <div>
+        <Button />
+      </div>
+    </section>
   )
 }
 
